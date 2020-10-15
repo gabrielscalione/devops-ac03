74674a66de7e4582ed27378db39bcba78dd6e373
Gabriel Scalione <69868688+gabrielscalione@users.noreply.github.com>
2020-10-15 20:13:14 -0300

    Update test_operacoes.py

--- com/gabriel/test/test_operacoes.py
+++ com/gabriel/test/test_operacoes.py
@@ -2,5 +2,5 @@ import pytest
 from com.gabriel.operacoes import operacoes
 
 def test_soma():
-  operacoes = Operacoes()
+  operacoes = operacoes()
   assert operacoes.soma([1,5]) == 6, "Should be 6"
